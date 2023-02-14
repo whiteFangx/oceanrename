df9151f307bc58dbb29d373db9ed63b4527a5b64
Riyan [wade] <86948336+whiteFangx@users.noreply.github.com>
2023-02-14 12:56:11 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -12,7 +12,7 @@ except ImportError:
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
         SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]

c3ab84b3ba3f3fbd88fdf62b7fb41022a679e479
Riyan [wade] <86948336+whiteFangx@users.noreply.github.com>
2023-02-14 12:54:57 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,10 +4,10 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://ok:ok@cluster0.ox6obby.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "f6c33f46599246676f75e153b615dbbc"]
+        API_ID = [int, 4783634]
+        BOT_TOKEN = [str, "6092663926:AAE124kuVPUZclLVZjiCT4-41O6ghIyrgPw"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
@@ -20,8 +20,8 @@ except ImportError:
         OWNER_ID = [int, 0]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN = [str,"t.me/fuckoff"]
+        FORCEJOIN_ID = [int,-1001745287248]
 
         TRACE_CHANNEL = [int, 0]
 
